f9d8692c702be75fe83be27aa5acfa59f77e32dc
KDH <kde6571@gmail.com>
2023-02-27 11:08:34 +0900

    Solve: problem 18108

---
+++ src/main/java/xyz/kdeDevelop/백준/입출력과_사칙연산/P8_18108/Main.java
@@ -0,0 +1,15 @@
+package xyz.kdeDevelop.백준.입출력과_사칙연산.P8_18108;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String input = bufferedReader.readLine();
+        bufferedReader.close();
+
+        System.out.println(Integer.parseInt(input) - 543);
+    }
+}
