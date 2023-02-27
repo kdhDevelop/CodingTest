100545a2cb6f624bd9036f0e29864abe465b7915
KDH <kde6571@gmail.com>
2023-02-27 10:58:47 +0900

    Solve: problem 1008

--- src/main/java/xyz/kdeDevelop/백준/입출력과_사칙연산/P5_1008/Main.java
+++ src/main/java/xyz/kdeDevelop/백준/입출력과_사칙연산/P5_1008/Main.java
@@ -1,4 +1,4 @@
-package xyz.kdeDevelop.백준.입출력과_사칙연산.P4_10998;
+package xyz.kdeDevelop.백준.입출력과_사칙연산.P5_1008;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +13,6 @@ public class Main {
         int a = Integer.parseInt(input[0]);
         int b = Integer.parseInt(input[1]);
 
-        System.out.println(a * b);
+        System.out.println((double) a / (double) b);
     }
 }
