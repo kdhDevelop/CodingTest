d913dc49afa1fe16d7da5f55d28e40ba38119bd9
KDH <kde6571@gmail.com>
2023-02-27 11:29:38 +0900

    Solve: problem 2588

---
+++ src/main/java/xyz/kdeDevelop/백준/입출력과_사칙연산/P10_2588/Main.java
@@ -0,0 +1,22 @@
+package xyz.kdeDevelop.백준.입출력과_사칙연산.P10_2588;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int inputA = Integer.parseInt(bufferedReader.readLine());
+        String inputB = bufferedReader.readLine();
+        bufferedReader.close();
+
+        for (int T = inputB.length() - 1 ; T >= 0 ; T --) {
+            int temp = Integer.parseInt(String.valueOf(inputB.charAt(T)));
+
+            System.out.println(inputA * temp);
+        }
+
+        System.out.println(inputA * Integer.parseInt(inputB));
+    }
+}
