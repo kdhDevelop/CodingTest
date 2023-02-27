ada13bbeaac938ac8a1f86a2cd5c15114864dcd9
KDH <kde6571@gmail.com>
2023-02-27 11:39:52 +0900

    Solve problem 10171

---
+++ src/main/java/xyz/kdeDevelop/백준/입출력과_사칙연산/P12_10171/Main.java
@@ -0,0 +1,10 @@
+package xyz.kdeDevelop.백준.입출력과_사칙연산.P12_10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
