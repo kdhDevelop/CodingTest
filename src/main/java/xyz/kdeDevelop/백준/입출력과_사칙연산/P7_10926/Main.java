0e76b10fe422fc1a894a421ba78173886d5df3ff
KDH <kde6571@gmail.com>
2023-02-27 11:05:07 +0900

    Solve: problem 10926

---
+++ src/main/java/xyz/kdeDevelop/백준/입출력과_사칙연산/P7_10926/Main.java
@@ -0,0 +1,15 @@
+package xyz.kdeDevelop.백준.입출력과_사칙연산.P7_10926;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String input = bufferedReader.readLine();
+        bufferedReader.close();
+
+        System.out.println(input.concat("??!"));
+    }
+}
