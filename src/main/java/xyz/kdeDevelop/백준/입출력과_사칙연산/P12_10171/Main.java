8fb28df6887dac1df702ee0fc7a255d971f5a5c1
KDH <kde6571@gmail.com>
2023-02-27 11:40:51 +0900

    Solve problem 10172

--- src/main/java/xyz/kdeDevelop/백준/입출력과_사칙연산/P12_10171/Main.java
+++ src/main/java/xyz/kdeDevelop/백준/입출력과_사칙연산/P12_10171/Main.java
@@ -2,9 +2,12 @@ package xyz.kdeDevelop.백준.입출력과_사칙연산.P12_10171;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\\    /\\\n" +
-                " )  ( ')\n" +
-                "(  /  )\n" +
-                " \\(__)|");
+        System.out.println("" +
+                "|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|" +
+                "");
     }
 }
