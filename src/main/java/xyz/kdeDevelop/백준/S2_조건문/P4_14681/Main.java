d38bec996cfb716a28b0d63086004767a691a8ee
KDH <kde6571@gmail.com>
2023-02-28 20:12:18 +0900

    Solve: problem 14681

---
+++ src/main/java/xyz/kdeDevelop/백준/S2_조건문/P4_14681/Main.java
@@ -0,0 +1,28 @@
+package xyz.kdeDevelop.백준.S2_조건문.P4_14681;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(bufferedReader.readLine());
+        int y = Integer.parseInt(bufferedReader.readLine());
+        bufferedReader.close();
+
+        if (x > 0) {
+            if (y > 0) {
+                System.out.println("1");
+            } else {
+                System.out.println("4");
+            }
+        } else {
+            if (y > 0) {
+                System.out.println("2");
+            } else {
+                System.out.println("3");
+            }
+        }
+    }
+}
