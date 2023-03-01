36011dca8fbe0a0106c726aa8c96c9b6e0293b6c
KDH <kde6571@gmail.com>
2023-03-01 22:45:46 +0900

    Solve: problem 15552

---
+++ src/main/java/xyz/kdeDevelop/백준/S3_반복문/P6_15552/Main.java
@@ -0,0 +1,25 @@
+package xyz.kdeDevelop.백준.S3_반복문.P6_15552;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int inputT = Integer.parseInt(bufferedReader.readLine());
+
+        for (int T = 0 ; T < inputT ; T ++) {
+            String[] input = bufferedReader.readLine().split(" ");
+
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
+
+            bufferedWriter.write((A + B) + "\n");
+        }
+
+        bufferedReader.close();
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
