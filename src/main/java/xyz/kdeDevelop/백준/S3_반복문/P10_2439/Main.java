f14ca4bc0b5af974f6e530f6e120f9a3484025c1
KDH <kde6571@gmail.com>
2023-03-01 23:03:25 +0900

    Solve: problem 2439

---
+++ src/main/java/xyz/kdeDevelop/백준/S3_반복문/P10_2439/Main.java
@@ -0,0 +1,30 @@
+package xyz.kdeDevelop.백준.S3_반복문.P10_2439;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = scanner.nextInt();
+
+        for (int T = 1 ; T <= N ; T ++) {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int TI = 0 ; TI < N - T ; TI ++)
+                stringBuilder.append(" ");
+            for (int TI = 0 ; TI < T ; TI ++)
+                stringBuilder.append("*");
+
+            stringBuilder.append("\n");
+            bufferedWriter.write(stringBuilder.toString());
+        }
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
