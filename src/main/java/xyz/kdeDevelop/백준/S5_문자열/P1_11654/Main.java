57a5833644fcd0e05155b81849e53b7d20fec562
KDH <kde6571@gmail.com>
2023-03-03 12:30:08 +0900

    Solve: problem 11654

---
+++ src/main/java/xyz/kdeDevelop/백준/S5_문자열/P1_11654/Main.java
@@ -0,0 +1,14 @@
+package xyz.kdeDevelop.백준.S5_문자열.P1_11654;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        System.out.println((int) input.charAt(0));
+
+        scanner.close();
+    }
+}
