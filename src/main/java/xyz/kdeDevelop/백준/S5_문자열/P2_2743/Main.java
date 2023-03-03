ab43ed07c3fcdf1162b8cb1e5fb09af388b36257
KDH <kde6571@gmail.com>
2023-03-03 12:35:09 +0900

    Solve: problem 2743

---
+++ src/main/java/xyz/kdeDevelop/백준/S5_문자열/P2_2743/Main.java
@@ -0,0 +1,15 @@
+package xyz.kdeDevelop.백준.S5_문자열.P2_2743;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        System.out.println(input.length());
+
+        scanner.close();
+    }
+}
