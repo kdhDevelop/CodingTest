eef9962b8768a3328d45d6a18132d55faaf93f30
KDH <kde6571@gmail.com>
2023-03-04 20:55:51 +0900

    Solve: problem 25083

---
+++ src/main/java/xyz/kdeDevelop/백준/S6_심화1/P1_25083/Main.java
@@ -0,0 +1,12 @@
+package xyz.kdeDevelop.백준.S6_심화1.P1_25083;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
